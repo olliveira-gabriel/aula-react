a3799b80d59c7223cf3b63fba2632abb0284612a
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2025-04-30 11:17:53 -0300

    Add files via upload

---
+++ src/pages/Adestramento.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Adestramento() {
+  return (
+    <div>
+       <h1>adestramento</h1>
+    </div>
+  )
+}
+
+export default Adestramento
---
+++ src/pages/BanhoTosa.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BanhoTosa() {
+  return (
+    <div>
+        <h1>Banho e Tosa</h1>
+    </div>
+  )
+}
+
+export default BanhoTosa
